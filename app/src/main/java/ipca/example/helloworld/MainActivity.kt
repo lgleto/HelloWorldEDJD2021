d422df1722f93e54113a31f01751afa213772fca
Lourenço Gomes <leto.lg@gmail.com>
2020-10-07 11:28:10 +0100

    - Added button - do translation

--- app/src/main/java/ipca/example/helloworld/MainActivity.kt
+++ app/src/main/java/ipca/example/helloworld/MainActivity.kt
@@ -2,14 +2,33 @@ package ipca.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+
         val textViewHello : TextView =  TextView(this)
+        val button = Button(this)
+        val linearLayout = LinearLayout(this)
+
         textViewHello.text = "Olá Mundo!"
-        setContentView(textViewHello)
+        button.text = "Traduzir"
+
+        linearLayout.addView(textViewHello)
+        linearLayout.addView(button)
+
+
+        button.setOnClickListener {
+            textViewHello.text = "Hello World!"
+        }
+
+
+        setContentView(linearLayout)
+
     }
 }
