15191538818586d57f1e6fc65f5310374347b325
Lourenço Gomes <leto.lg@gmail.com>
2020-10-07 12:02:13 +0100

    - Same thing using XML

--- app/src/main/java/ipca/example/helloworld/MainActivity.kt
+++ app/src/main/java/ipca/example/helloworld/MainActivity.kt
@@ -2,33 +2,14 @@ package ipca.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-
-        val textViewHello : TextView =  TextView(this)
-        val button = Button(this)
-        val linearLayout = LinearLayout(this)
-
-        textViewHello.text = "Olá Mundo!"
-        button.text = "Traduzir"
-
-        linearLayout.addView(textViewHello)
-        linearLayout.addView(button)
-
-
-        button.setOnClickListener {
-            textViewHello.text = "Hello World!"
+        setContentView(R.layout.activity_main)
+        buttonTranslation.setOnClickListener {
+            textViewHello.text = "Olá mundo!"
         }
-
-
-        setContentView(linearLayout)
-
     }
 }
